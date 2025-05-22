3dfd88badc0fceaff2e872adb32f1f64ba9e7575
Sandali Samarawickrama <wathmali123@gmail.com>
2025-05-23 09:50:35 +1000

    temp fix

--- todo-service/index.js
+++ todo-service/index.js
@@ -16,7 +16,7 @@ const logger = winston.createLogger({
   ],
 });
 
-const MONGO_URI = process.env.MONGODB_URI;
+const MONGO_URI = "mongodb://todo-user:securepassword@mongo:27017/todoapp?authSource=admin";
 mongoose.connect(MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => logger.info("MongoDB connected for Todo Service"))
   .catch(err => logger.error("MongoDB connection error:", err));
--- user-service/index.js
+++ user-service/index.js
@@ -22,7 +22,8 @@ const logger = winston.createLogger({
   ],
 });
 
-const MONGO_URI = process.env.MONGODB_URI;
+//const MONGO_URI = process.env.MONGODB_URI;
+const MONGO_URI= "mongodb://todo-user:securepassword@mongo:27017/todoapp?authSource=admin";
 mongoose.connect(MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => logger.info("MongoDB connected for User Service"))
   .catch(err => logger.error("MongoDB connection error:", err));

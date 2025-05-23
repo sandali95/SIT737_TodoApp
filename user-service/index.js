62451b1c9f648370adf50e9b5dfd2188b692ca94
Sandali Samarawickrama <wathmali123@gmail.com>
2025-05-23 16:23:05 +1000

    Fix: add unit tests step to CI/CD workflows and update MongoDB URI to use environment variable

--- user-service/index.js
+++ user-service/index.js
@@ -22,8 +22,7 @@ const logger = winston.createLogger({
   ],
 });
 
-//const MONGO_URI = process.env.MONGODB_URI;
-const MONGO_URI= "mongodb://todo-user:securepassword@mongo:27017/todoapp?authSource=admin";
+const MONGO_URI = process.env.MONGODB_URI;
 mongoose.connect(MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => logger.info("MongoDB connected for User Service"))
   .catch(err => logger.error("MongoDB connection error:", err));
